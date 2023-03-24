99e4775d610ba06ad8cef801c06c2fa366417d9f
lili.farkas <lili.farkas14@gmail.com>
2023-03-24 13:06:58 +0100

    Refactor App.js and create routes

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import EquipmentList from "./components/equipmentList";
+import CreateEquipment from "./components/createEquipment";
+import EditEquipment from "./components/editEquipment";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<RecordList />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/equipment" element={<EquipmentList />} />
+          <Route path="/createequipment" element={<CreateEquipment/>} />
+          <Route path="/equipment/edit/:id" element={<EditEquipment/>} />
+        </Routes>
+      </div>
   );
-}
+};
 
 export default App;
