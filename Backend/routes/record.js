916b78da54fe673ae7a2227c974d9503089d2813
lili.farkas <lili.farkas14@gmail.com>
2023-03-24 01:47:11 +0100

    Create get record by id route

--- Backend/routes/record.js
+++ Backend/routes/record.js
@@ -5,20 +5,27 @@ const express = require("express");
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const recordRoutes = express.Router();
 
-// This will help us connect to the database
 const dbo = require("../db/connection");
 
-// This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-
-// This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
     let db_connect = dbo.getDb("employees");
     db_connect
         .collection("records")
         .find({})
         .toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
+recordRoutes.route("/record/:id").get(function (req, res) {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect
+        .collection("records")
+        .findOne(myquery, function (err, result) {
             if (err) throw err;
             res.json(result);
         });
