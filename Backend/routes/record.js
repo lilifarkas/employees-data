fda90b7f1f912136f1e17611ea738c9ca710060c
lili.farkas <lili.farkas14@gmail.com>
2023-03-24 01:48:42 +0100

    Create delete record route

--- Backend/routes/record.js
+++ Backend/routes/record.js
@@ -68,3 +68,15 @@ recordRoutes.route("/update/:id").post(function (req, response) {
             response.json(res);
         });
 });
+
+recordRoutes.route("/:id").delete((req, response) => {
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+        if (err) throw err;
+        console.log("1 document deleted");
+        response.json(obj);
+    });
+});
+
+module.exports = recordRoutes;
