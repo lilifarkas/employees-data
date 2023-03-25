535da59d2b3ab279e79731ec5cac1ebe08693386
lili.farkas <lili.farkas14@gmail.com>
2023-03-25 20:03:10 +0100

    Change the connection from Mongo Atlas to Mongo Compass

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
+
+const uri = "mongodb://localhost:27017/employees-data";
+const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -8,18 +9,44 @@ const client = new MongoClient(Db, {
 let _db;
 
 module.exports = {
-    connectToServer: function (callback) {
-        client.connect(function (err, db) {
-            if (db)
-            {
-                _db = db.db("employees");
-                console.log("Successfully connected to MongoDB.");
-            }
-            return callback(err);
-        });
+    connectToServer: async function (callback) {
+        try {
+            await client.connect();
+            _db = client.db();
+            console.log("Successfully connected to MongoDB.");
+            callback();
+        } catch (err) {
+            callback(err);
+        }
     },
 
     getDb: function () {
         return _db;
     },
 };
+
+// const { MongoClient } = require("mongodb");
+// const Db = process.env.ATLAS_URI;
+// const client = new MongoClient(Db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+//
+// let _db;
+//
+// module.exports = {
+//     connectToServer: function (callback) {
+//         client.connect(function (err, db) {
+//             if (db)
+//             {
+//                 _db = db.db("employees");
+//                 console.log("Successfully connected to MongoDB.");
+//             }
+//             return callback(err);
+//         });
+//     },
+//
+//     getDb: function () {
+//         return _db;
+//     },
+// };
