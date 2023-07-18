25150ffcc7f798e0e8b49934a996ad66b96c4dd6
lili.farkas <lili.farkas14@gmail.com>
2023-07-18 17:14:00 +0200

    Fix the url

--- frontend/client/src/constants/constanUrl.jsx
+++ frontend/client/src/constants/constanUrl.jsx
@@ -1,3 +1,3 @@
 // const URL = 'http://localhost:5000/';
-const URL = 'https://employees-data-backend.onrender.com';
+const URL = 'https://employees-data-backend.onrender.com/';
 export default URL;
