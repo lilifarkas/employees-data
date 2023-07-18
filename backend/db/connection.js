5877813c394eb248186a89cc18d9f9e6673fca54
lili.farkas <lili.farkas14@gmail.com>
2023-07-18 15:31:27 +0200

    Change the Mongodb connection to Atlas

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require("mongodb");
-
-const uri = "mongodb://localhost:27017/employees-data";
+const uri = process.env.ATLAS_URI;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
